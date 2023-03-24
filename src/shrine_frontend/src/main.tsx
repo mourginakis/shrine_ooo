34acbcfe6ba664d49cc985ec7f4bbeff423dbe31
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-03-23 17:20:40 -0700

    delete all the unnecessary autogenerated stuff

--- src/shrine_frontend/src/main.tsx
+++ src/shrine_frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Counter from './Counter';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter />
+    <App />
   </React.StrictMode>,
-)
+);
