7df566399c293afce1aaab1f13eaaf8179701831
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-03-23 19:14:02 -0700

    refine comments for authentication

--- src/shrine_frontend/src/ExampleWhoami.tsx
+++ src/shrine_frontend/src/ExampleWhoami.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Actor, HttpAgent } from '@dfinity/agent';
+import { Actor, HttpAgent, ActorSubclass } from '@dfinity/agent';
 import { Secp256k1KeyIdentity } from "@dfinity/identity-secp256k1";
 import { Principal } from "@dfinity/principal";
 
 
 
-// ----------------- BEGIN ----------------- //
+// ----------------- BEGIN AUTH ----------------- //
 
-// You can either import the actor from the bakcend like so (not currently working):
+// You can either import the actor from the backend like so
+// not currenlty working due to the fetchRootKey() call returning a parsing error:
 
 // console.log(process.env.SHRINE_BACKEND_CANISTER_ID);
 // import { idlFactory, shrine_backend } from '../../declarations/shrine_backend';
@@ -18,30 +19,30 @@ import { Principal } from "@dfinity/principal";
 
 
 // You can create your own actor in here like so:
-
+// for some reason, importing from ../../declarations/shrine_backend
+// calls fetchRootKey, which gives a weird parse error that I think has to
+// do with some globals thing.
 // @ts-expect-error
 import { idlFactory } from '../../declarations/shrine_backend/shrine_backend.did.js';
+import { _SERVICE } from '../../declarations/shrine_backend/shrine_backend.did';
 
 
-// use the authed testing identity
+// use the authed testing identity, or set identity to null to use the anonymous identity
 const identity = Secp256k1KeyIdentity.fromParsedJson(
     STATIC_CONTEXT.testingIdentityJSON
 );
 
-// or use the anonymous identity, "shrine_backend"
-
-
 const agent = new HttpAgent({ host: STATIC_CONTEXT.host, identity: identity});
 if (STATIC_CONTEXT.network === "local") { agent.fetchRootKey() };
 
-const shrine_backend = Actor.createActor(
+const shrine_backend: ActorSubclass<_SERVICE> = Actor.createActor(
     idlFactory,
     { agent, canisterId: STATIC_CONTEXT.canisterId }
 );
 
 
-
 // ----------------- END ----------------- //
+
 
 
 
@@ -52,7 +53,7 @@ const ExampleWhoami: React.FC<Props> = () => {
 
     const callWhoami = async () => {
         const result = await shrine_backend.whoami();
-        let result2 = result as Principal; // this should be done automatically but the import is broken
+        let result2 = result;
         setWhoami(result2.toString());
     };
 
