fc71d3545268abf319339ea6729b3aa70323f381
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-04-04 21:25:41 -0700

    add tests intended to fail

--- src/shrine_backend_tests_ts/trove.test.ts
+++ src/shrine_backend_tests_ts/trove.test.ts
@@ -1,13 +1,42 @@
-import { assert, describe, expect, it } from 'vitest';
+import { assert, describe, expect, it, test } from 'vitest';
 
 import { shrine_backend_anony, shrine_backend_authed } from './actors';
 
 
-
-describe('trove', () => {
-    it('does_nothing', async () => {
-        expect(1).toEqual(1);
+describe('createTrove', () => {
+    it('returns error on anonymous user', async () => {
+        const result = await shrine_backend_anony.createTrove();
+        expect(result).toEqual("Error: You must be logged in to create a trove.");
     });
-})
 
+    it('succeeds on authed user', async () => {
+        const result = await shrine_backend_authed.createTrove();
+        expect(result).toEqual("Trove successfully created.");
+    });
+
+});
+
+
+
+describe('revalueTrove', () => {
+    it.skip('test not yet implemented', async () => {
+        expect(true).toEqual(false);
+    });
+});
+
+
+
+describe('dissolveTrove', () => {
+    it.skip('test not yet implemented', async () => {
+        expect(true).toEqual(false);
+    });
+});
+
+
+
+describe('listTroves', () => {
+    it.skip('test not yet implemented', async () => {
+        expect(true).toEqual(false);
+    });
+});
 
